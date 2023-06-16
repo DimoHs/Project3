c04702ee28cc7e7fb9f210542e0b961d8741ce7a
Piromanski <hard.style@abv.bg>
2023-06-17 01:26:48 +0300

    First File Committed

---
+++ src/com/company/Computer.java
@@ -0,0 +1,54 @@
+package com.company;
+
+class Computer {
+    private String brand;
+    private String model;
+    private int productionYear;
+    private int priceBgn;
+    private int ramMemoryGB;
+    private int hddMemoryGB;
+    private String operatingSystem;
+
+
+
+
+    Computer(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    Computer(String brand, String model, int priceBgn) {
+        this.brand = brand;
+        this.model = model;
+        this.priceBgn = priceBgn;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+    public int getPriceBgn() {
+        return this.priceBgn;
+    }
+
+
+
+    public String getDetails() {
+        String output = ("Brand:"+getBrand()+"\n" +"Model:"+getModel());
+
+        return output;
+    }
+    public String getDetails2() {
+        String output2 = ("Brand:"+getBrand()+"\n" +"Model:"+getModel()+"\n"+"Price:"+getPriceBgn());
+
+        return output2;
+    }
+}
+
+
+
+
+
