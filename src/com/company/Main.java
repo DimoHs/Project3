7a78ed9827c035fe7dc4d7d790f79ecbf3982b78
Piromanski <hard.style@abv.bg>
2023-06-17 02:11:01 +0300

    Third File Committed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         Computer computer = new Computer("Acer","Aspire");
-        Computer computer2 = new Computer("Acer","Aspire",1000);
+        Computer computer2 = new Computer("Acer","Aspire",1001);
 
         String object1 = computer.getDetails();
         System.out.println(object1);
