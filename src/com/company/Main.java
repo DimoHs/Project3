1885202fc9d7888406c7edfb52e8c5c629867961
Piromanski <hard.style@abv.bg>
2023-06-17 01:37:29 +0300

    Second File Committed

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+
+        Computer computer = new Computer("Acer","Aspire");
+        Computer computer2 = new Computer("Acer","Aspire",1000);
+
+        String object1 = computer.getDetails();
+        System.out.println(object1);
+
+        String object2 = computer2.getDetails2();
+        System.out.println(object2);
+
+    }
+}
